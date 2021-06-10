c99cf5cceda843d46adeb15d7dae984abc0f0fd2
cortlandniccum <cortlandniccum@gmail.com>
2021-06-10 16:11:38 -0600

    finished tasks 3, 4, and 5

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ const reviews = [
 Using the reviews array above:
   1. log only Julius' feedback to the console - no function needed 
 */
-
+console.log('task 3', reviews[5].feedback);
 
 
 
@@ -87,7 +87,9 @@ Using the reviews array above do the following: (no function needed)
   1. Following the same format (name, rating, feedback), add a new fictitious review object to the reviews array
   2. log the whole array to the console, make sure the new review is inside of it   
 */
+reviews.push({name: 'Cortland', rating: 4.7, feedback: 'This is my favorite place to eat!'});
 
+console.log('task 4', reviews);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -96,6 +98,8 @@ Reyna's feedback is missing! Use what you know to do the following: (no function
   2. log the reviews array to the console to check your work
 */
 
+reviews[7].feedback = 'this place is chill with really cool people, great for getting work done on weekdays';
+console.log('task 5', reviews);
 
 
 
